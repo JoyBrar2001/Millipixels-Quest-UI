64a0023d2b0e9d7b9d20882ac043fde63d57f497
Joy Brar <joybrar2001@gmail.com>
2023-07-20 10:23:46 +0530

    Fixed the form

--- app/components/admin-components/AddQuestionForm.tsx
+++ app/components/admin-components/AddQuestionForm.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import { BiMinus } from 'react-icons/bi';
+import { TiTick } from 'react-icons/ti'
 
 const AddQuestionForm = () => {
 
   const [questionType, setQuestionType] = useState('');
   const [options, setOptions] = useState(['']);
+  const [answer, setAnswer] = useState('None');
+
+  const handleCorrectOption = (option: string) => {
+    setAnswer(option);
+  }
 
   const handleOptionChange = (index: number, value: string) => {
     const updatedOptions = [...options];
@@ -19,7 +25,7 @@ const AddQuestionForm = () => {
 
   const handleRemoveOption = (index: number) => {
     const updatedOptions = [...options];
-    updatedOptions.splice(index,1);
+    updatedOptions.splice(index, 1);
     setOptions(updatedOptions);
   }
 
@@ -57,29 +63,41 @@ const AddQuestionForm = () => {
         </form>
 
         {(questionType === "MCQ" || questionType === "Draggable") && (
-        <form className="flex-center w-full items-start flex-col gap-2 my-12" >
-          <h2 className='text-2xl font-semibold text-[#666a71] border-b-[1px] border-b-[#1f1f1f]'>Add Options Here</h2>
-          {options.map((option,index) => (
-            <div className="flex-center gap-2" key={index}>
-              <input className="block px-4 py-2 outline-none bg-transparent border-b-[1px] border-b-[#1f1f1f] focus:bg-white focus:border-b-cyan-500 focus:border-b-2 focus:rounded-xl focus:placeholder-cyan-500" type="text" placeholder={`Add Option ${index+1}`} value={option} onChange={(e) => handleOptionChange(index, e.target.value)}/>
-              {index >= 0 && (
-                <button 
-                  onClick={() => handleRemoveOption(index)}
-                  className="bg-white rounded-full text-cyan-500 px-2 py-1 shadow-lg shadow-cyan-500/30 font-bold text-5xl"
-                >
-                  <BiMinus size={15} className='relative' />
-                </button>
-              )}
-            </div>
+          <form className="flex-center w-full items-start flex-col gap-2 my-12" >
+            <h2 className='text-2xl font-semibold text-[#666a71] border-b-[1px] border-b-[#1f1f1f]'>Add Options Here</h2>
+            {options.map((option, index) => (
+              <div className="flex-center gap-2" key={index}>
+                <input className="block px-4 py-2 outline-none bg-transparent border-b-[1px] border-b-[#1f1f1f] focus:bg-white focus:border-b-cyan-500 focus:border-b-2 focus:rounded-xl focus:placeholder-cyan-500" type="text" placeholder={`Add Option ${index + 1}`} value={option} onChange={(e) => handleOptionChange(index, e.target.value)} />
+                {index >= 0 && (
+                  <>
+                    <button
+                      type='button'
+                    onClick={() => handleCorrectOption(option)}
+                      className={` rounded-full  px-2 py-1 shadow-lg shadow-cyan-500/30 font-bold text-5xl ${option === answer ? 'bg-cyan-500 text-white' : 'bg-white text-cyan-500'}`}
+                      >
+                      <TiTick size={15} className='relative' />
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => handleRemoveOption(index)}
+                      className="bg-white rounded-full text-cyan-500 px-2 py-1 shadow-lg shadow-cyan-500/30 font-bold text-5xl"
+                    >
+                      <BiMinus size={15} className='relative' />
+                    </button>
+                  </>
+                )}
+              </div>
             ))}
-          <button 
-            type="button" 
-            onClick={handleAddOption} 
-            className="bg-white rounded-full text-cyan-500 text-sm mt-4 px-4 py-2 shadow-lg shadow-cyan-500/30"
-          >
-            Add Option
-          </button>
-        </form>
+            <button
+              type="button"
+              onClick={handleAddOption}
+              className="bg-white rounded-full text-cyan-500 text-sm mt-4 px-4 py-2 shadow-lg shadow-cyan-500/30"
+            >
+              Add Option
+            </button>
+
+            <h3 className='bg-white rounded-full text-cyan-500 mt-4 px-4 py-2 shadow-lg shadow-cyan-500/30'>Correct Answer : {answer || 'None'}</h3>
+          </form>
         )}
       </div>
 
